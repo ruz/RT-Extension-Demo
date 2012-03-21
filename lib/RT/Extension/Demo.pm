4612ec016bec14aeb1c531edf81e9b5ac5d365c0
Ruslan Zakirov <ruz@bestpractical.com>
2012-03-21 19:55:53 +0400

    first demo version
    presented here http://request-tracker.ru/node/669

---
+++ lib/RT/Extension/Demo.pm
@@ -0,0 +1,33 @@
+use 5.008003;
+use strict;
+use warnings;
+
+package RT::Extension::Demo;
+
+our $VERSION = '0.01'
+
+=head1 NAME
+
+RT::Extension::Demo - demo of an extension for RT
+
+=head1 SYNOPSIS
+
+    Set(@Plugins, qw(RT::Extension::Demo));
+
+=head1 DESCRIPTION
+
+=cut
+
+# ... for code ...
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>ruslan.zakirov@gmail.comE<gt>
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=cut
+
+666;
